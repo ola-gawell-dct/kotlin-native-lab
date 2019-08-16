463c60437f3febf975cfc838ea2c6ff8b19e3187
Ola Gawell <ola.gawell@jayway.com>
2019-08-16 09:48:54 +0200

    Started from scratch to write the README

--- app/src/main/java/sample/ApplicationDispatcher.kt
+++
@@ -1,6 +0,0 @@
-package sample
-
-import kotlinx.coroutines.*
-
-internal actual val DispatcherIO: CoroutineDispatcher = Dispatchers.IO
-internal actual val DispatcherMain: CoroutineDispatcher = Dispatchers.Main
